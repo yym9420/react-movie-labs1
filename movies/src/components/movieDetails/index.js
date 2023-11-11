930e454be25f7c0cef2bb0765a2116bb160de400
Yiming Yu <2693604213@qq.com>
2023-11-11 23:18:02 +0000

    Movie Details Page - included production countries.

--- movies/src/components/movieDetails/index.js
+++ movies/src/components/movieDetails/index.js
@@ -57,6 +57,16 @@ const MovieDetails = ( props) => {
         />
         <Chip label={`Released: ${movie.release_date}`} />
       </Paper>
+<Paper component="ul" sx={{ ...root }}>
+        <li>
+          <Chip label="Production Countries" sx={{ ...chip }} color="primary" />
+        </li>
+        {movie.production_countries.map((country) => (
+          <li key={country.name}>
+            <Chip label={country.name} sx={{ ...chip }} />
+          </li>
+        ))}
+      </Paper>
       <Fab
         color="secondary"
         variant="extended"

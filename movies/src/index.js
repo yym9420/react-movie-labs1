ae6c8ee8897c6d96161bf45e511abcc397c8e6d8
Yiming Yu <2693604213@qq.com>
2023-11-12 16:51:29 +0000

    Full review page.

--- movies/src/index.js
+++ movies/src/index.js
@@ -1,3 +1,4 @@
+import MovieReviewPage from "./pages/movieReviewPage";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router-dom";
@@ -21,6 +22,7 @@ const App = () => {
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={ <Navigate to="/" /> } />
+        <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
       </Routes>
     </BrowserRouter>
   );

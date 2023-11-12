c99ba0134a1bedcc6bbb2a3a85a180a358dcc902
Yiming Yu <2693604213@qq.com>
2023-11-12 16:46:24 +0000

    Show movie review excerpts; Refactor Details page.

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -1,3 +1,14 @@
+export const getMovieReviews = (id) => {
+  return fetch(
+    
+`https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${process.env.REACT_APP_TMDB_KEY}`
+  )
+    .then((res) => res.json())
+    .then((json) => {
+      // console.log(json.results);
+      return json.results;
+    });
+};
 export const getMovies = () => {
   return fetch(
     `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
--- movies/src/components/movieDetails/index.js
+++ movies/src/components/movieDetails/index.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import Drawer from "@mui/material/Drawer";
+import MovieReviews from "../movieReviews"
 import Chip from "@mui/material/Chip";
 import Paper from "@mui/material/Paper";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
@@ -19,8 +21,8 @@ const root = {
 };
 const chip = { margin: 0.5 };
 
-const MovieDetails = ( props) => {
-  const movie = props.movie
+const MovieDetails = ({ movie }) => {  // Don't miss this!
+const [drawerOpen, setDrawerOpen] = useState(false);
 
   return (
     <>
@@ -70,15 +72,20 @@ const MovieDetails = ( props) => {
       <Fab
         color="secondary"
         variant="extended"
+        onClick={() =>setDrawerOpen(true)}
         sx={{
-            position: "fixed",
-            bottom: 2,
-            right: 2
+          position: 'fixed',
+          bottom: '1em',
+          right: '1em'
         }}
       >
         <NavigationIcon />
         Reviews
       </Fab>
+      <Drawer anchor="top" open={drawerOpen} onClose={() => 
+setDrawerOpen(false)}>
+        <MovieReviews movie={movie} />
+      </Drawer>
       </>
   );
 };

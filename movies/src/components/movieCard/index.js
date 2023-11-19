d47f45955733d43ac947434300472685ee552963
Yiming Yu <2693604213@qq.com>
2023-11-19 08:35:30 +0000

    Bug Fix - make action icons on movie card configurable.

--- movies/src/components/movieCard/index.js
+++ movies/src/components/movieCard/index.js
@@ -15,7 +15,7 @@ import Grid from "@mui/material/Grid";
 import img from '../../images/film-poster-placeholder.png'
 import React, { useContext  } from "react";
 import { MoviesContext } from "../../contexts/moviesContext";
-export default function MovieCard({ movie }) {
+export default function MovieCard({ movie,action }) {
   const { favorites, addToFavorites } = useContext(MoviesContext);
 
   if (favorites.find((id) => id === movie.id)) {
@@ -69,9 +69,7 @@ export default function MovieCard({ movie }) {
         </Grid>
       </CardContent>
       <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites" onClick={handleAddToFavorite}>
-        <FavoriteIcon color="primary" fontSize="large" />
-    </IconButton>
+        {action(movie)}
         <Link to={`/movies/${movie.id}`}>
           <Button variant="outlined" size="medium" color="primary">
             More Info ...
--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -3,6 +3,7 @@ import { getMovies } from "../api/tmdb-api";
 import PageTemplate from '../components/templateMovieListPage';
 import { useQuery } from 'react-query';
 import Spinner from '../components/spinner';
+import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
 
 const HomePage = (props) => {
 
@@ -24,11 +25,12 @@ getMovies)
   const addToFavorites = (movieId) => true 
 
   return (
-    <PageTemplate
-      title='Discover Movies'
-      movies={movies}
-      selectFavorite={addToFavorites}
-    />
-  );
-};
+      <PageTemplate
+        title="Discover Movies"
+        movies={movies}
+        action={(movie) => {
+          return <AddToFavoritesIcon movie={movie} />
+        }}
+      />
+  );}
 export default HomePage;
